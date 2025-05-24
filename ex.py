b693c4d411987facc89ffde02f09b9316fdb52bc
NsubugaIbrahim <ibrahimnazel942@gmail.com>
2025-05-24 22:46:07 +0300

    Assignment 2B

--- ex.py
+++ ex.py
@@ -1,3 +1,10 @@
-print("Enter two numbers")
-x = input("Enter the first number")
-y = input("Enter the second number")
+def division(x, y):
+    return x/y
+def main():
+    print("Enter two numbers to find their quotient. (x/y)")
+    x = float(input("Enter the first number x: "))
+    y = float(input("Enter the second numbery: "))
+    result = division(x, y)
+    print(f"The quotient of {x} and {y} is {result}")
+if __name__ == "__main__":
+    main()
