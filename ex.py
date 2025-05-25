f9fbccb2c3dcaed81a2e1cee2085bca575e35946
NsubugaIbrahim <ibrahimnazel942@gmail.com>
2025-05-25 17:39:07 +0300

    Exceptions

--- ex.py
+++ ex.py
@@ -4,7 +4,12 @@ def main():
     print("Enter two numbers to find their quotient. (x/y)")
     x = float(input("Enter the first number x: "))
     y = float(input("Enter the second numbery: "))
-    result = division(x, y)
-    print(f"The quotient of {x} and {y} is {result}")
+    try:
+        result = division(x, y)
+        print(f"The quotient of {x} and {y} is {result}")
+    except ZeroDivisionError:
+        print("Error: Division by zero is not allowed.")
+    except ValueError:
+        print("Please enter valid numbers.")
 if __name__ == "__main__":
     main()
