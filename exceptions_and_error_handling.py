e5facba33d37578ac7807f196084e229e268f928
NsubugaIbrahim <ibrahimnazel942@gmail.com>
2025-05-25 17:56:30 +0300

    Break statement

--- exceptions_and_error_handling.py
+++ exceptions_and_error_handling.py
@@ -8,6 +8,7 @@ def main():
         try:
             result = division(x, y)
             print(f"The quotient of {x} and {y} is {result}")
+            break # Exit the loop if division is successful
         except ZeroDivisionError:
             print("Error: Division by zero is not allowed.")
         except ValueError:
